4e13e518b7558a8a1d65194435020c9d369669a9
Haazem66 <hazem_hashem@outlook.com>
2023-05-06 01:00:09 +0200

    create and new actions in controller and added a flash message when creating a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,13 +7,24 @@ class PostsController < ApplicationController
     end
 
     def new
-        
+        #to make available instance for error checking
+        @post = Post.new
     end
 
     def create
+        #getting the required info from the form
         @post = Post.new(params.require(:post).permit(:title, :description))
-        @post.save
-        redirect_to post_path(@post)
+
+        if 
+        
+       if @post.save
+        #redirecting to the new made post
+            flash[:notice] = "Post was created successfully."
+            redirect_to post_path(@post)
+       else
+        #rendering the form again and displaying error messages
+           render 'new'
+       end
     end
 
 end
