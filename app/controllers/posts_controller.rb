4acbc967e5d863ae2c391e17f4d147a198ce7f3a
Haazem66 <hazem_hashem@outlook.com>
2023-05-07 04:31:07 +0200

    some code refactoring DRY

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,46 +1,52 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
     def show
-        @post = Post.find(params[:id])
     end
+  
     def index
-        @posts = Post.all
+      @posts = Post.all
     end
-    
-    def edit
-        @post = Post.find(params[:id])
-    end
-
+  
     def new
-        #to make available instance for error checking
-        @post = Post.new
+      @post = Post.new
     end
-
+  
+    def edit
+    end
+  
     def create
-        #getting the required info from the form
-        @post = Post.new(params.require(:post).permit(:title, :description))
-
-        
-       if @post.save
-        #redirecting to the new made post
-            flash[:notice] = "Post was created successfully."
-            redirect_to post_path(@post)
-       else
-        #rendering the form again and displaying error messages
-           render 'new'
-       end
+      @post = Post.new(post_params)
+      if @post.save
+        flash[:notice] = "Article was created successfully."
+        redirect_to @post
+      else
+        render 'new'
+      end
     end
-
+  
     def update
-        @post = Post.find(params[:id])
-        if @post.update(params.require(:post).permit(:title, :description))
-            flash[:notice] = "Post was updated successfully"
-            redirect_to @post
-        else
-            render 'edit'
-        end
-
-        
-
+      if @post.update(post_params)
+        flash[:notice] = "Article was updated successfully."
+        redirect_to @post
+      else
+        render 'edit'
+      end
     end
-
-end
+  
+    def destroy
+      @post.destroy
+      redirect_to posts_path
+    end
+  
+    private
+  
+    def set_post
+      @post = Post.find(params[:id])
+    end
+  
+    def post_params
+      params.require(:post).permit(:title, :description)
+    end
+  
+  end
