9a73d02b3cac3fdf655c7033e88950b6c445b5cf
Haazem66 <hazem_hashem@outlook.com>
2023-05-10 22:04:35 +0200

    added index action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
 
+
+
+    def index
+        @users = User.all
+    end
+
+    
     def new
       @user = User.new
+    end
+
+    def show
+        @user = User.find(params[:id])
+        @posts = @user.posts
     end
   
     def edit
