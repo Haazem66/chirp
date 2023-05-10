f7871695a98e3f6448d06c00bae33a13bb0a29e2
Haazem66 <hazem_hashem@outlook.com>
2023-05-10 21:42:00 +0200

    edit user template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,25 @@ class UsersController < ApplicationController
       @user = User.new
     end
   
+    def edit
+      @user = User.find(params[:id])
+    end
+  
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:notice] = "Your account information was successfully updated"
+        redirect_to posts_path
+      else
+        render 'edit'
+      end
+    end
+  
     def create
       @user = User.new(user_params)
       if @user.save
         flash[:notice] = "Welcome to Chirp #{@user.username}, you have successfully signed up"
-        redirect_to posts_path
+        redirect_to articles_path
       else
         render 'new'
       end
