f520e794404e0f1ef8fb136e74b081cdb6144d9a
Haazem66 <hazem_hashem@outlook.com>
2023-05-11 02:47:47 +0200

    added admin field to users table and some modification and restrictions to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,47 +1,68 @@
 class UsersController < ApplicationController
-
-
+    before_action :set_user, only: [:show, :edit, :update , :destroy]
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update , :destroy]
+  
+    def show
+        @user = User.find(params[:id])
+        @posts = @user.posts
+    end
+  
     def index
         @users = User.all
     end
-
+  
     def new
       @user = User.new
     end
-
-    def show
-        @user = User.find(params[:id])
-       @posts = @user.posts
-    end
   
     def edit
-      @user = User.find(params[:id])
+        @user = User.find(params[:id])
     end
   
     def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        flash[:notice] = "Your account information was successfully updated"
-        redirect_to @user
-      else
-        render 'edit'
-      end
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+          flash[:notice] = "Your account information was successfully updated"
+          redirect_to @user
+        else
+          render 'edit'
+        end
     end
   
     def create
-      @user = User.new(user_params)
-      if @user.save
-        session[:user_id] = @user.id
-        flash[:notice] = "Welcome to Chirp #{@user.username}, you have successfully signed up"
-        redirect_to posts_path
-      else
-        render 'new'
-      end
+        @user = User.new(user_params)
+        if @user.save
+          session[:user_id] = @user.id
+          flash[:notice] = "Welcome to Chirp #{@user.username}, you have successfully signed up"
+          redirect_to posts_path
+        else
+          render 'new'
+        end
+    end
+
+    def destroy
+        @user.destroy
+        session[:user_id] = nil if @user == current_user
+        flash[:notice] = "Account and all associated posts successfully deleted"
+        redirect_to root_path
+
     end
   
     private
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+  
+    def set_user
+      @user = User.find(params[:id])
+    end
+  
+    def require_same_user
+        if current_user != @user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your own account"
+        redirect_to @user
+      end
     end
     
   end
